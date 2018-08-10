91b0082b9e0c2b77dfb65cfe7beee76ae71e54f8
frank <franklin.benitez@loyola.edu.mx>
2018-08-10 03:30:40 -0500

    New and useful classes for handling currency

--- PrimerPlugin/src/main/java/primero/Currency.java
+++ PrimerPlugin/src/main/java/primero/Currency.java
@@ -10,22 +10,70 @@ import org.bukkit.inventory.ItemStack;
 public class Currency implements Listener{
 	
 	@EventHandler
-	public void onCheck(PlayerInteractEvent event) {
+	public void onCheckCoins(PlayerInteractEvent event) {
+		
 		Player player = (Player) event.getPlayer();
+		Material itemType = player.getInventory().getItemInMainHand().getType();
+		
+		if (isCurrency(itemType)) {
+		
+			int coins = howMuch(player, Material.GOLD_NUGGET);
+			player.sendMessage("You have " + coins + " pieces of gold");
+		}
+			
+		
+	}
+	
+	//With this function you can check how much material a player currently has
+	public int howMuch(Player player, Material material) {
+		
 		ItemStack actualItem = player.getInventory().getItemInMainHand();
-		//Here I check the inventory of the player to check how much GOLD_INGOT he's carrying
-		if(actualItem.getType().equals(Material.GOLD_INGOT)) {
-			int coins = 0;
-
+		int coins = 0;
+		
+		//check the inventory of the player for how much material he's carrying if he has a old ingot in his hand
+		if(actualItem.getType().equals(material)) {
+			
+			//here we iterate over every slot of the player inventory
 			for(int i = 0; i <= player.getInventory().getContents().length; i++) {
+				
 				if (player.getInventory().getItem(i) != null) {
-					if (player.getInventory().getItem(i).getType().equals(Material.GOLD_INGOT)){
+					
+					//We detect if the current inventory slot has gold ingot and if true the amount of the stack 
+					if (player.getInventory().getItem(i).getType().equals(material)){
+						
+						//we add 1 coin for every ingot
 						coins = coins + player.getInventory().getItem(i).getAmount();
 					}
 				}
 			}
-			player.sendMessage("You have " + coins + " pieces of gold");
 		}
+		return coins;
+	}
+	
+	public boolean isCurrency(Material material){
+	//Checks if the material send is currency or not
+		
+		boolean isMoney = false;
+		Material[] currency = { Material.GOLD_INGOT, Material.GOLD_NUGGET, Material.IRON_INGOT, Material.IRON_NUGGET };
+		
+		if(material != null) {
+		
+			for (Material moneytype : currency) {
+			
+				if ( material.equals(moneytype) ) {
+		    	
+					isMoney = true;
+					break;
+		    	
+				}else {
+		    	
+					isMoney = false;
+					
+				}
+			}
+		}
+	
+		return isMoney;
 	}
 
 }
