7a5cc13bd5dc58a34c7a3f5228e11a8ea9599bce
frank <franklin.benitez@loyola.edu.mx>
2018-08-19 03:55:51 -0500

    working database not the best.

--- PrimerPlugin/src/main/java/primero/CustomCommand.java
+++ PrimerPlugin/src/main/java/primero/CustomCommand.java
@@ -1,5 +1,13 @@
 package primero;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,12 +16,58 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.plugin.Plugin;
 
 public class CustomCommand implements CommandExecutor, Listener{
 
-	boolean selectingItem = false;
 	public String cmd1 = "market";
+	public String cmd2 = "price";
 	CustomInventory market = new CustomInventory();
+	boolean selectingItem = false;
+	Connection con = PrimerPlugin.getPlugin(PrimerPlugin.class).getConnection();
+	Plugin objplugin = PrimerPlugin.getPlugin(PrimerPlugin.class);
+	private String table;
+
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		Player senderPlayer = (Player) sender;
+		
+		if ( command != null) {
+			if (command.getName().equals(cmd1)) {
+				market.openMarket(senderPlayer);
+			}else if (command.getName().equals(cmd2)) {
+				int itemId = Integer.parseInt( args[1] );
+				float price =  Float.parseFloat( args[0] ) ;
+				try {
+					updatePrice( price, itemId);
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public void updatePrice(float price, int id) {
+
+		table = PrimerPlugin.getPlugin(PrimerPlugin.class).table;
+
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Editando el item " + id );
+		PreparedStatement updateItemPrice;
+		try {
+			updateItemPrice = con.prepareStatement("UPDATE mainmarket SET price=? WHERE id=?");
+			updateItemPrice.setFloat(1, price);
+			updateItemPrice.setInt(2, id);
+			updateItemPrice.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	@EventHandler
 	public void onPlayerDragsInventory(InventoryDragEvent event) {
@@ -27,17 +81,36 @@ public class CustomCommand implements CommandExecutor, Listener{
 	public void onPlayerClicksInventory(InventoryClickEvent event) {
 
 		String clickedItemName = event.getCurrentItem().getItemMeta().getDisplayName();
+		String clickedItemType = event.getCurrentItem().getType().toString();
+		int clickedItemQuantity = event.getCurrentItem().getAmount();
+		float defaultprice = 1000;
 		Player activePlayer = (Player) event.getWhoClicked();
+		String table = PrimerPlugin.getPlugin(PrimerPlugin.class).table;
 		String inventoryClicked = event.getInventory().getName();
 		
-		if (selectingItem == true && event.getCurrentItem() != null && clickedItemName != "SEND ITEM") {
-			activePlayer.sendMessage( event.getCurrentItem().getType().toString() );
+		if (selectingItem == true && clickedItemName != "SEND ITEM") {
+		//when clicked on SEND ITEM you activated SELECTING ITEM
+			activePlayer.sendMessage( "added "+ clickedItemQuantity + " "+ event.getCurrentItem().getType().toString() + " to market");
+			// debugging line
+			try {
+				PreparedStatement addItemToMarket = con.prepareStatement("INSERT INTO " + table + " (NAME, OWNER, QUANTITY, PRICE) VALUES (?,?,?,?)");
+				addItemToMarket.setString(1, clickedItemType);
+				addItemToMarket.setString(2, activePlayer.getDisplayName());
+				addItemToMarket.setInt(3, clickedItemQuantity);
+				addItemToMarket.setFloat(4, defaultprice);
+				addItemToMarket.executeUpdate();
+				Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "adding to market database...");
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			selectingItem = false;
-			
+				
 		}
 		
-		if( clickedItemName == "SEND ITEM" && selectingItem == false ){
-			
+		if( clickedItemName == "SEND ITEM" && selectingItem == false ){	
+		//Triggers when player clicks on SEND ITEM button
 			selectingItem = true;
 			event.setCancelled(true);
 
@@ -56,19 +129,6 @@ public class CustomCommand implements CommandExecutor, Listener{
 			event.setCancelled(true);
         }
 
-	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
-		Player senderPlayer = (Player) sender;
-		
-		if ( command != null) {
-			if (command.getName().equals(cmd1)) {
-				market.openMarket(senderPlayer);
-			}
-		}
-		
-		return false;
 	}
 
 }
--- PrimerPlugin/src/main/java/primero/PrimerPlugin.java
+++ PrimerPlugin/src/main/java/primero/PrimerPlugin.java
@@ -1,22 +1,77 @@
 package primero;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class PrimerPlugin extends JavaPlugin{
+	
+	private Connection connection;
+	public String host, database, username, password, table;
+	public int port;
 	
     // Fired when plugin is first enabled
     @Override
     public void onEnable() {
+    	
+    	//mysql connection
+    	mysqlSetup();
     	
     	CustomCommand command = new CustomCommand();
     	
         getServer().getPluginManager().registerEvents(new Currency(), this);
         getServer().getPluginManager().registerEvents(new CustomCommand(), this);
         this.getCommand(command.cmd1).setExecutor(command);
+        this.getCommand(command.cmd2).setExecutor(command);
     }
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
 
     }
+
+	public void mysqlSetup(){
+		host = "localhost";
+		port = 3306;
+		database = "minecraftserver";
+		username = "minecraftserver";
+		password = "Gamerz";
+		table = "mainmarket";
+		
+		try{
+			
+			synchronized (this){
+				if(getConnection() != null && !getConnection().isClosed()){
+					return;
+				}
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				setConnection( DriverManager.getConnection("jdbc:mysql://" + this.host + ":" 
+				+ this.port + "/" + this.database, this.username, this.password));
+				
+				Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MYSQL CONNECTED");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	} 
+	
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+    
 }
